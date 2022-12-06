073de7d25f85111c272417d71b7649ca716d3b14
Hızır Mutlu <mhizir58@gmail.com>
2022-12-06 21:15:09 +0300

    Basic Math lesson with python

---
+++ Basic_Math.py
@@ -0,0 +1,25 @@
+# Math Operators
+
+#Symbol    Name 
+#  +       Sum
+#  -       Extraction
+#  *       Impact
+#  /       Divide
+#  **      Pow
+# '//'     Full Divide
+
+
+print(3+4) # sum int 
+print(3.0+4) # sum float 
+print(3*3) # int
+print(3.0*3) #float
+print(10/5) #float 2.0
+print(3+5*2) #result is 13
+print(3+5*6/2)#result 18.0 float
+print(2**3) # result is 8 because 2*2*2 pow funciton
+print(10%5) # result is 0
+print(11%2) #result is 1
+print(12//5) #result is 2
+print((12//5)**2) # result is 4. Because 12//5=2 2*2=4
+
+
